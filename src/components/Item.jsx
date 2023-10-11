d7f51d057708dec7fe5a579472223ca9a7d6b3bc
donguk717 <donguk717@gmail.com>
2023-10-11 14:22:16 +0900

    서버주소변경

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -14,7 +14,7 @@ const Item = ({ imgSrc, name, position ,index}) => {
 
         <div className='item-item' onClick={() => {
             console.log("Detail 요청")
-            let url = "http://localhost:8089/SpringBoot2/PlayerDetail.do?name="+name;
+            let url = "http://3.135.228.75:8089/SpringBoot2/PlayerDetail.do?name="+name;
             
             axios.get(url)
             .then((res)=>{
